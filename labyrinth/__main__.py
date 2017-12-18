ea9090c52c7181d2fefc4d5d44f3d0ec4e4a933d
Lorenzo Mureu <mureulor@gmail.com>
2017-12-18 16:50:11 +0100

    Implemented commandline options handling

--- labyrinth/__main__.py
+++ labyrinth/__main__.py
@@ -1,12 +1,13 @@
 from labyrinth.labyrinth import Labyrinth
 from labyrinth.solver import *
+from labyrinth.config import Configure
 from time import sleep
 import curses
 from sys import argv
 from copy import deepcopy
 
-def main(screen, win1, win2):
-    l1 = Labyrinth(r,c,p)
+def main(screen, conf, win1, win2):
+    l1 = Labyrinth(conf['rows'], conf['cols'], conf['prob'])
     l2 = deepcopy(l1)
     s1 = DFS_Solver(l1)
     s2 = BFS_Solver(l2)
@@ -16,7 +17,12 @@ def main(screen, win1, win2):
         r1 = run(win1, s1, "DFS")
         r2 = run(win2,s2,"BFS")
         flag = r1 or r2
+        sleep(conf['delay'])
 
+    win1.addstr(0,0,"DFS: {}, {}".format(s1.count, s1.status))
+    win1.refresh()
+    win2.addstr(0,0,"BFS: {}, {}".format(s2.count, s2.status))
+    win2.refresh()
     screen.addstr(curses.LINES-1, 0, "Press any key to exit... ")
     curses.curs_set(1)
     screen.getch()
@@ -29,27 +35,18 @@ def run(win, solver, message):
     win.addstr(1,0,str(solver.labyrinth))
     win.refresh()
     #input("Press ENTER key to continue...")
-    sleep(s)
     return True
 
-DEFAULT_ROWS = 15
-DEFAULT_COLUMNS = 40
-DEFAULT_PROBABILITY = 0.15
-DEFAULT_SPEED = 0.005
 
-
-if len(argv) == 5:
-    r,c,p,s = int(argv[1]), int(argv[2]), float(argv[3]), float(argv[4])
-elif len(argv) == 4:
-    r,c,p,s = int(argv[1]), int(argv[2]), float(argv[3]), DEFAULT_SPEED
-elif len(argv) == 3:
-    r,c,p,s = int(argv[1]), int(argv[2]), DEFAULT_PROBABILITY, DEFAULT_SPEED
-else:
-    r,c,p,s = DEFAULT_ROWS, DEFAULT_COLUMNS, DEFAULT_PROBABILITY, DEFAULT_SPEED
 
 screen = curses.initscr()
-curses.noecho()
+LINES, COLS = curses.LINES, curses.COLS
+curses.endwin()
+
+configurator = Configure(LINES, COLS)
+
+curses.initscr()
 curses.curs_set(0)
-win1 = curses.newwin(curses.LINES, (curses.COLS-1)//2, 0, 0)
-win2 = curses.newwin(curses.LINES, (curses.COLS-1)//2, 0, (curses.COLS+1)//2)
-curses.wrapper(main, win1, win2)
+win1 = curses.newwin(LINES, COLS//2, 0, 0)
+win2 = curses.newwin(LINES, COLS//2, 0, COLS//2+1)
+curses.wrapper(main, configurator.conf, win1, win2)
---
+++ labyrinth/config.py
@@ -0,0 +1,62 @@
+from sys import argv as args
+
+class Configure(object):
+    DEFAULT_PROBABILITY = 0.15
+    DEFAULT_DELAY = 0.005
+
+    def __init__(self, height, width):
+        self.height = height
+        self.width  = width
+        self.maxrows = self.height - 2
+        self.maxcols =  (self.width-1)//2
+        self.conf = {
+            'rows': self.maxrows,
+            'cols': self.maxcols,
+            'delay': Configure.DEFAULT_DELAY,
+            'prob': Configure.DEFAULT_PROBABILITY,
+        }
+        self.__parse_args__()
+        self.__validate__()
+
+    def __parse_args__(self):
+        i = 1
+        while i < len(args):
+            if args[i].startswith('--'):
+                arg = args[i].replace('--', '')
+                if arg not in self.conf.keys():
+                    self.usage()
+                self.conf[arg] = args[i+1]
+                i += 2
+            else: self.usage()
+
+    def usage(self):
+        print("""Usage: {} [option [value]] ...
+
+--rows rows         -- Labyrinth's number of rows
+--cols columns      -- Labyrinth's number of columns
+--delay delay       -- Delay after a move (in seconds). Default: {}
+--prob probability  -- Probability of walls. Default: {}""".format(
+                            args[0],
+                            Configure.DEFAULT_DELAY,
+                            Configure.DEFAULT_PROBABILITY))
+        exit(1)
+
+    def __validate__(self):
+        self.conf['rows']  =  int(self.conf['rows'])
+        self.conf['cols']  =  int(self.conf['cols'])
+        self.conf['prob']  =  float(self.conf['prob'])
+        self.conf['delay'] =  float(self.conf['delay'])
+
+        if not 3 <= self.conf['rows'] <= self.maxrows:
+            print("Invalid number of rows, using max:", self.maxrows)
+            self.conf['rows'] = self.maxrows
+
+        if not 3 <= self.conf['cols'] <= self.maxcols:
+            print("Invalid number of columns, using max:", self.maxcols)
+            self.conf['cols'] = self.maxcols
+
+        if self.conf['delay'] < 0: self.conf['delay'] *= -1
+
+        if not 0 <= self.conf['prob'] <= 1:
+            print("Probabilities must belong in interval [0,1], using default")
+            self.conf['prob'] = Configure.DEFAULT_PROBABILITY
